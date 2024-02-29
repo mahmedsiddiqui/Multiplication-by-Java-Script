e9481dd2bc06f213af36e778d80e6216f04a46f8
Muhammad Ahmed Siddiqui <154745545+mahmedsiddiqui@users.noreply.github.com>
2024-02-29 11:12:32 +0500

    Add files via upload

---
+++ multiply in javascript/app.js
@@ -0,0 +1,4 @@
+const num1 = parseInt(prompt('Enter Your First Number To multiply'));
+const num2 = parseInt(prompt('Enter Your Second Number To multiply '));
+const sum = num1 * num2;
+document.write(`Your Sum is ${sum}`); 
